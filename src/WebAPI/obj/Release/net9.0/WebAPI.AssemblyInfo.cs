e9c2d22668af7a92d98a34cfd9c61b43257b811e
MohammedBouchtaoui <mohammedbouchtaoui48@gmail.com>
2026-09-14 13:26:20 +0100

    feat(backend): implémentation de la Clean Architecture, EF Core et Product API

--- src/WebAPI/obj/Release/net9.0/WebAPI.AssemblyInfo.cs
+++ src/WebAPI/obj/Release/net9.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3529a7ec2d00b39aee5c814f029de9ff34587dc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
